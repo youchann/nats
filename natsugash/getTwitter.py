0a11d429d8d42a06ba09c430f4cf7da536446cd1
ryoya247 <chan1no.hello.16@gmail.com>
2018-08-29 03:47:53 +0900

    media_typeで振り分け url削除済みtextでvoicefile生成できるように mainpage.htmlの微修正 動画含むツイートは動画を再生できるように

--- natsugash/getTwitter.py
+++ natsugash/getTwitter.py
@@ -12,7 +12,7 @@ twitter = OAuth1Session(CK, CS, AT, ATS)
 def get_tweets (name):
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
     params = {
-        'count': 20,
+        'count': 25,
         'screen_name': name,
         'exclude_replies': True,
         'include_rts': False
@@ -23,19 +23,39 @@ def get_tweets (name):
 
 def assort_tweets (timelines):
     tweets = {}
+
     for line in timelines:
+        tweet_id = line['id_str']
         text = line['text']
-        if (text.find('https') != -1):
-            text = text[:text.find('https')]
+        # if (text.find('https') != -1):
+        #     text = text[:text.find('https')]
+        photo_num = 0
+        media_type = "none"
+        media_src = [""]
 
         if ('extended_entities' in line):
-            pprint.pprint (line['extended_entities']['media'])
+            for media in line['extended_entities']['media']:
+                if (media['type'] == "photo"):
+                    media_type = "photo"
+                    photo_num += 1
+                    media_src.append(media['media_url'])
 
-        try:
-            image = line['extended_entities']['media'][0]['media_url']
-            tweets[text] = image
-        except KeyError:
-            tweets[text] = "画像なしツイート"
+                elif (media['type'] == "video"):
+                    pprint.pprint(media)
+                    media_type = "video"
+                    media_src.append(media['video_info']['variants'][2]['url'])
+
+        # try:
+        #     image = line['extended_entities']['media'][0]['media_url']
+        #     tweets[tweet_id] = image
+        # except KeyError:
+        #     tweets[text] = "画像なしツイート"
+        tweets[tweet_id] = {
+            'text' : text,
+            'photo_num' : photo_num,
+            'media_type' : media_type,
+            'media_src' : media_src,
+        }
 
     return tweets
 
