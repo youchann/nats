084432ea00d5738e8afc834b7c8502d1cd1e7706
ryoya247 <chan1no.hello.16@gmail.com>
2018-09-11 03:26:03 +0900

    test

--- manage.py
+++ manage.py
@@ -2,6 +2,6 @@ from natsugash import app
 import os
 
 # app.run(host='127.0.0.1', port=8080, debug=True)
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT'))
 print(os.environ.get('PORT'))
 app.run(port=port)
