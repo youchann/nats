d6bb30e1b7d6a0b1570aa10aeb3c0d8bfe7f87d9
ryoya247 <chan1no.hello.16@gmail.com>
2018-11-04 05:41:52 +0900

    [WIP]refactoring pacman

--- natsugash/views.py
+++ natsugash/views.py
@@ -62,3 +62,16 @@ def show_del_tweets():
     db.child("tweets").push(delTweets)
     session.clear()
     return render_template('delpac.html')
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
